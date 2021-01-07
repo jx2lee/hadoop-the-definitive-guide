def84009c982c97a6790fbb0ffda25b83ab28ec7
jx2lee <jaejun.lee.1991@gmail.com>
2021-01-08 00:40:44 +0900

    Edit max_temperature.sh

--- chapter02/max_temperature.sh
+++ chapter02/max_temperature.sh
@@ -7,7 +7,7 @@ if [ -z $dir_name ];
 then
    echo "[ERROR] set data directory"
    echo "usage:" >&2
-   echo "    $0 ncdc_data" >&2
+   echo "    $0 all" >&2
    exit 1
 fi
 
@@ -15,7 +15,6 @@ function main() {
     for year in $run_dir/$dir_name/*
     do
       printf `basename $year .gz`'\t'
-      #/bin/echo -ne `basename $year .gz` "\t"
       gunzip -c $year | \
         awk '{ temp = substr($0, 88, 5) + 0;
                q = substr($0, 93, 1);
